49bf89632baaeb8792726389e206f0f9c2464eca
mihir-rathod <mihirr.rathodd@gmail.com>
2026-03-04 13:36:24 -0500

    spring boot gateway skeleton

---
+++ gateway-service/src/main/java/com/diplomat/gateway/DiplomatGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.diplomat.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomatGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomatGatewayApplication.class, args);
+	}
+
+}
